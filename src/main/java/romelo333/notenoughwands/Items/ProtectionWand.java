7491e79a50dcecbab51cc0d0e64573c578c6c5f3
McJty <mcjty1@gmail.com>
2015-10-18 13:09:05 +0200

    Added usage registration. Made availability creative only.

--- src/main/java/romelo333/notenoughwands/Items/ProtectionWand.java
+++ src/main/java/romelo333/notenoughwands/Items/ProtectionWand.java
@@ -1,14 +1,17 @@
 package romelo333.notenoughwands.Items;
 
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import romelo333.notenoughwands.ProtectedBlocks;
 
 public class ProtectionWand extends GenericWand{
     public ProtectionWand() {
-        setup("ProtectionWand", "protectionWand").xpUsage(10).availability(AVAILABILITY_ADVANCED).loot(4);
+        setup("ProtectionWand", "protectionWand").xpUsage(10).availability(AVAILABILITY_CREATIVE).loot(0);
     }
 
     @Override
@@ -18,9 +21,19 @@ public class ProtectionWand extends GenericWand{
             if (player.isSneaking()) {
                 protectedBlocks.unprotect(world,x,y,z);
             } else {
+                if (!checkUsage(stack, player, 1.0f)) {
+                    return true;
+                }
                 protectedBlocks.protect(world,x,y,z);
+                registerUsage(stack, player, 1.0f);
             }
         }
         return true;
     }
+
+    @Override
+    protected void setupCraftingInt(Item wandcore) {
+        GameRegistry.addRecipe(new ItemStack(this), "re ", "ew ", "  w", 'r', Items.comparator, 'e', Items.ender_eye, 'w', wandcore);
+    }
+
 }
